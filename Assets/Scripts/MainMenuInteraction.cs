d9cc746a9b6705b2124d4f9667ffcc52eed396e6
monahm7 <mona.heidarimoghadam@gmail.com>
2026-09-06 11:50:27 +0300

    MainMenu UI

--- Assets/Scripts/MainMenuInteraction.cs
+++ Assets/Scripts/MainMenuInteraction.cs
@@ -7,6 +7,7 @@ public class MainMenuInteraction : MonoBehaviour
     public Camera menuCamera;
     public Transform startPlanet;
     public Transform quitPlanet;
+    public Transform[] decorativeObjects;
 
     [Header("Hover")]
     public float hoverScale = 1.03f;
@@ -15,16 +16,25 @@ public class MainMenuInteraction : MonoBehaviour
     Transform _hoveredPlanet;
     Vector3 _startScale;
     Vector3 _quitScale;
+    Vector3[] _decorativeScales;
 
     void Awake()
     {
         if (menuCamera == null)
             menuCamera = Camera.main;
 
+        if (decorativeObjects == null)
+            decorativeObjects = new Transform[0];
+
         if (startPlanet != null)
             _startScale = startPlanet.localScale;
         if (quitPlanet != null)
             _quitScale = quitPlanet.localScale;
+
+        _decorativeScales = new Vector3[decorativeObjects != null ? decorativeObjects.Length : 0];
+        for (int i = 0; i < _decorativeScales.Length; i++)
+            if (decorativeObjects[i] != null)
+                _decorativeScales[i] = decorativeObjects[i].localScale;
     }
 
     void Update()
@@ -37,6 +47,8 @@ public class MainMenuInteraction : MonoBehaviour
                 StartGame();
             else if (_hoveredPlanet == quitPlanet)
                 QuitGame();
+            else
+                Debug.Log($"[FirstLight] Menu object selected: {_hoveredPlanet.name}");
         }
     }
 
@@ -49,16 +61,39 @@ public class MainMenuInteraction : MonoBehaviour
             Ray ray = menuCamera.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
-                if (startPlanet != null && hit.transform.IsChildOf(startPlanet))
+                if (IsTarget(hit.transform, startPlanet))
                     target = startPlanet;
-                else if (quitPlanet != null && hit.transform.IsChildOf(quitPlanet))
+                else if (IsTarget(hit.transform, quitPlanet))
                     target = quitPlanet;
+                else
+                {
+                    for (int i = 0; i < decorativeObjects.Length; i++)
+                    {
+                        if (IsTarget(hit.transform, decorativeObjects[i]))
+                        {
+                            target = decorativeObjects[i];
+                            break;
+                        }
+                    }
+                }
             }
         }
 
         _hoveredPlanet = target;
         AnimateScale(startPlanet, _startScale, target == startPlanet);
         AnimateScale(quitPlanet, _quitScale, target == quitPlanet);
+
+        for (int i = 0; i < _decorativeScales.Length; i++)
+        {
+            Transform decorativeObject = decorativeObjects[i];
+            if (decorativeObject != null)
+                AnimateScale(decorativeObject, _decorativeScales[i], target == decorativeObject);
+        }
+    }
+
+    bool IsTarget(Transform hitTransform, Transform target)
+    {
+        return target != null && (hitTransform == target || hitTransform.IsChildOf(target));
     }
 
     void AnimateScale(Transform planet, Vector3 baseScale, bool hovered)
